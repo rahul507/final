a3b37ab0b0352aaf917af055d5d764f76f4e7fb4
Garvit2207 <garvitgarg1995@gmail.com>
2023-03-24 00:17:00 +0530

    update

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import io
 import os
 from PIL import Image
 import datetime
+import cv2
+import numpy as np
 
 import torch
 from flask import Flask, render_template, request, redirect, jsonify
@@ -26,8 +28,23 @@ def predict():
             return render_template("fail.html")
 
         img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        results = model([img])
+        im = Image.open(io.BytesIO(img_bytes))
+        padded_img = np.array(im)
+        # padded_img = [padded_img]
+        old_image_height, old_image_width, channels = padded_img.shape
+        # print(old_image_width)
+        new_image_height = old_image_height
+        new_image_width = old_image_width + 600
+        color = (255,255,255)
+        result = np.full((new_image_height,new_image_width, channels), color, dtype=np.uint8)
+        x_center = (new_image_width - old_image_width) // 2
+        y_center = (new_image_height - old_image_height) // 2
+
+# copy img image into center of result image
+        result[y_center:y_center+old_image_height, 
+        x_center:x_center+old_image_width] = padded_img
+        pil_image = Image.fromarray(result)
+        results = model([result])
 
         results.render()  # updates results.imgs with boxes and labels
         now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
@@ -44,6 +61,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
-    model = torch.hub.load('ultralytics/yolov5','custom', path='best.pt')
+    model = torch.hub.load('ultralytics/yolov5','custom', path='C:/Users/garvi/OneDrive/Desktop/YOLO Training/yolov5/run_120/weights/best.pt')
     model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
